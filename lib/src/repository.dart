edf5241f7a87f51829113f820e498d04cd65d810
msiviero <m.siviero83@gmail.com>
2021-03-13 18:24:28 +0100

    Added docs

--- lib/src/repository.dart
+++ lib/src/repository.dart
@@ -3,27 +3,33 @@ import 'package:quiver/core.dart';
 
 const int maxInt = 9007199254740992;
 
+/// A repository of type T
 class Repository<T> {
   final Box<T> _box;
 
   Repository(this._box);
 
+  /// Puts an [item] in the box at the given [key]
   Future<void> put(key, T item) {
     return _box.put(key, item);
   }
 
+  /// Add an [item] to the box. In this case, the key will be computed
   Future<void> add(T item) {
     return _box.add(item);
   }
 
+  /// Returns an optional wrapping a single item, found (or not) by [key]
   Optional<T> find(String key) {
     return Optional.fromNullable(_box.get(key));
   }
 
+  /// Deletes an item at the given [key]
   Future<void> delete(String key) {
     return _box.delete(key);
   }
 
+  /// Returns a stream of max [limit] values
   Stream<T> stream({int limit = maxInt}) async* {
     var i = 0;
     for (final key in _box.keys) {
@@ -35,8 +41,10 @@ class Repository<T> {
     }
   }
 
+  /// Returns a list of max [limit] values
   Future<List<T>> list({int limit = maxInt}) => stream(limit: limit).toList();
 
+  /// Returns the number of items contained in the box
   int count() => _box.keys.length;
 }
 
@@ -45,22 +53,27 @@ class LazyRepository<T> {
 
   LazyRepository(this._box);
 
+  /// Puts an [item] in the box at the given [key]
   Future<void> put(key, T item) {
     return _box.put(key, item);
   }
 
+  /// Add an [item] to the box. In this case, the key will be computed
   Future<void> add(T item) {
     return _box.add(item);
   }
 
+  /// Returns an optional wrapping a single item, found (or not) by [key]
   Future<Optional<T>> find(String key) async {
     return Optional.fromNullable(await _box.get(key));
   }
 
+  /// Deletes an item at the given [key]
   Future<void> delete(String key) {
     return _box.delete(key);
   }
 
+  /// Returns a stream of max [limit] values
   Stream<T> stream({int limit = maxInt}) async* {
     var i = 0;
     for (final key in _box.keys) {
@@ -73,7 +86,9 @@ class LazyRepository<T> {
     }
   }
 
+  /// Returns a list of max [limit] values
   Future<List<T>> list({int limit = maxInt}) => stream(limit: limit).toList();
 
+  /// Returns the number of items contained in the box
   int count() => _box.keys.length;
 }
